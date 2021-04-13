fbc75c1a59065d86361396b8f93fdc81d8cd5fdb
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2021-04-13 15:17:54 -0300

    R130420211517

---
+++ app/Http/Livewire/PruebaVehiculos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PruebaVehiculos extends Component
+{
+    public function render()
+    {
+        return view('livewire.prueba-vehiculos');
+    }
+}
