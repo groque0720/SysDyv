f36e916286f75af382c18f9b616464602f424a8a
Elias Ravarotto <elias_rvt@hotmail.com>
2021-04-16 10:35:12 -0300

    Elias

--- app/Http/Livewire/PruebaVehiculos.php
+++ app/Http/Livewire/PruebaVehiculos.php
@@ -6,8 +6,27 @@ use Livewire\Component;
 
 class PruebaVehiculos extends Component
 {
+
+	public $boletos = [];
+
     public function render()
     {
+
+    	$username = "USdqOwO7vYWqOiVqKdJm";
+		$password = "mA3wJbiPgcXnTZg7y05R";
+		$host = "http://200.7.15.135:9101/dcx/api/boletos?created_since=2010-05-11T08:00:00.000Z&detailed=true";
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $host);
+		curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		// dd($response);
+        $this->boletos = json_decode($response);
+
         return view('livewire.prueba-vehiculos');
     }
 }
--- resources/views/livewire/prueba-vehiculos.blade.php
+++ resources/views/livewire/prueba-vehiculos.blade.php
@@ -1,4 +1,10 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
     hola desde prueba
+
+    @foreach($boletos as $v)
+    <ul>
+    	<li>{{ $v->Cabecera->NroBoleto }}</li>
+    </ul>
+    @endforeach
 </div>
